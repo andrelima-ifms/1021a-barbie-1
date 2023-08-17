8023dbb7312bbda33f3e183e102b809bbebae422
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-08-17 08:13:35 -0400

    Adicionando fotos no projeto

--- playground-3.mongodb.js
+++ playground-3.mongodb.js
@@ -1,5 +1,5 @@
-
-use("tere");
-//db.createCollection("pessoa");
-//db.createCollection("curso");
-db.createCollection("qualquercoisa")
+//Filtrando dados com mysql
+// SELECT * from personal WHERE _id=1;
+//Filtrando dados no mongodb
+use("bdacademia");
+db.personal.find({_id:1},{salario:true})
