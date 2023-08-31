7c2ef1d89544ce080c0ccf414d8d865498d5f8a7
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-08-31 07:32:35 -0400

    React Router Dom

--- playground-3.mongodb.js
+++ playground-3.mongodb.js
@@ -1,5 +1,33 @@
-//Filtrando dados com mysql
-// SELECT * from personal WHERE _id=1;
-//Filtrando dados no mongodb
+
 use("bdacademia");
-db.personal.find({_id:1},{salario:true})
+db.dropDatabase();
+use("bdacademia");
+db.createCollection("personal");
+db.personal.insertMany([
+    {_id:1,
+        nome:"Júlio",
+        salario:3000, 
+        alunos:[
+            {
+                nome:"Tere",
+                idade:28
+            },
+            {
+                nome:"João",
+                idade:34
+            },
+            {
+                nome:"Pedro",
+                idade:60
+            }
+        ]
+    },
+    {_id:2,nome:"Adriano",salario:3000,alunos:[]},
+    {_id:3,nome:"Thaina",salario:4000,alunos:[]}
+])
+
+//Como apagar algum dado de uma collection
+// db.personal.drop() //Apaga uma collection
+// db.personal.find()
+
+db.personal.deleteMany({})
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import Header from './componentes/header/Header'
 import Filme from './componentes/filme/Filme'
 import './App.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header></Header>,
+  },
+  {
+    path: "/filme",
+    element: <div>Filme</div>,
+  },
+]);
 
 function App() {
   return (
-    <>
-      <Header/>
-      <Filme/>
-    </>
+    <RouterProvider router={router}/>
   )
 }
 export default App
