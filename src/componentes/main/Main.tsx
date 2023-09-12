90805bcece7da7d6fcf5d79cc291410e86da6976
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-09-12 09:41:28 -0400

    Construção do TrataTexto

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react'
 import Filme from './../filme/Filme'
 import './Main.css'
 export default function Main() {
-    let textodigitado = 'Barbie'
-    function TrataTexto(){
-        console.log("Digitado")
+    //let textodigitado = 'Barbie'
+    //Hooks são funções do React que ajudam a gente a fazer tarefas
+    //específicas
+    const [textodigitado,funcaoMudaVariavel]=useState("Barbie")
+
+    //O parâmetro "e" da minha função será o meu evento que ocorreu
+    function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
+        console.log(e.target.value)
         //Como eu faço para mudar o texto para "TERE"
-        textodigitado = "TERE"
+        funcaoMudaVariavel("TERE")
     }
     return (
         <>
