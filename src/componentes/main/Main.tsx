62c7973dc771efceeaa6ddf8eff240a5f739d2d4
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-09-12 10:12:32 -0400

    Atividade para próxima aula

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,17 +1,45 @@
 import { useState } from 'react'
 import Filme from './../filme/Filme'
 import './Main.css'
+type FilmeType = {
+    id:number,
+    titulo:string,
+    sinopse:string,
+    imagem:string
+}
+
 export default function Main() {
     //let textodigitado = 'Barbie'
     //Hooks são funções do React que ajudam a gente a fazer tarefas
     //específicas
-    const [textodigitado,funcaoMudaVariavel]=useState("Barbie")
+    const [texto,setTexto]=useState("")
+
+    const filmes:FilmeType[] = [
+        {
+            id:1,
+            titulo:'Barbie',
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:'/barbie.png'
+        },
+        {
+            id:2,
+            titulo:'Filme Barbie',
+            sinopse:'Depois de ser expulsa da Barbieland por ser.',
+            imagem:'/KEN.png'
+        },
+        {
+            id:3,
+            titulo:'Filme Barbie',
+            sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
+            imagem:'/boneca.jpg'
+        }
+    ]
 
     //O parâmetro "e" da minha função será o meu evento que ocorreu
     function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
-        console.log(e.target.value)
+        //console.log(e.target.value)
         //Como eu faço para mudar o texto para "TERE"
-        funcaoMudaVariavel("TERE")
+        setTexto(e.target.value)
     }
     return (
         <>
@@ -21,10 +49,16 @@ export default function Main() {
                        className='botao_pesquisa'
                        placeholder='Pesquise um Filme'
                        onChange={TrataTexto} />
-                <p>Digitado: {textodigitado} </p>
+                {texto && <p>Resultados Para: {texto} </p>}
             </div>
             <main className="content-main">
-                <Filme titulo='Barbie'
+                {/* 
+                    Use algo do vetor para tentar criar os filmes 
+                */}
+
+
+                
+                {/* <Filme titulo='Barbie'
                     sinopse='Depois de ser expulsa da 
                    Barbieland por ser uma boneca de aparência 
                    menos do que perfeita, Barbie parte para o 
@@ -77,7 +111,7 @@ export default function Main() {
                    menos do que perfeita, Barbie parte para o 
                    mundo humano em busca da verdadeira felicidade.'
                     imagem='/boneca.jpg'
-                />
+                /> */}
 
             </main>
         </>
